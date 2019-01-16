22008cf1fcb204f0c551973b5d5d172ee52aee95
jdujaka <jdujaka@gmail.com>
2019-01-16 23:30:21 +0100

    init redux store structure

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
+import store from "./js/store";
+import { addUser } from "./js/actions";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: ""
-    };
-
-  }
-  render() {
-    return (
-        <h1>Main App</h1>
-    );
-  }
-}
-
-
-const wrapper = document.getElementById("app");
-wrapper ? ReactDOM.render(<App />, wrapper) : false;
-	
-export default App; 
+window.store = store;
+window.addArticle = addUser;
---
+++ src/js/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+  users: []
+};
+
+function rootReducer(state = initialState, action) {
+  return state;
+};
+
+export default rootReducer;
